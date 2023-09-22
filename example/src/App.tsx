5653f7d26343cd29a087f3ad14dd88ee224b5250
Claudio Matos <claudiomatos@MacBook-Air-de-Claudio.local>
2023-09-21 21:53:37 -0300

    feat: first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'http-module';
+import { api } from 'http-module';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    const loadProducts = async () => {
+      const response = await api.get('/read-products');
+      // console.log(`RESPOSTA >>> ${JSON.stringify(response)}`);
+    };
+    loadProducts();
   }, []);
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,17 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+// Para instalar as bibliotecas no modulo
+// npm install --save-dev
+
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL:
+    'https://master--relaxed-fenglisu-464c83.netlify.app/.netlify/functions',
+});
+
+// import { IProduct } from '../common/product-interface';
+// import api from './api';
+
+// export const fetchProducts = async (): Promise<IProduct[]> => {
+//   const response = await api.get<IProduct[]>('/read-products');
+//   return response.data;
+// };
